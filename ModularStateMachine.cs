3f1d77f8f7d3e5c0c204ae80d69256d78bca7238
SteveSteiner <stvstnr@googlemail.com>
2021-03-14 09:52:35 +0100

    Finish Repo

---
+++ ModularStateMachine.cs
@@ -0,0 +1,53 @@
+﻿public class ModularStateMachine
+{
+    #region Variables
+    public IState CurrentState
+    {
+        get
+        {
+            return currentState;
+        }
+    }
+    public IState PreviousState
+    {
+        get
+        {
+            return previousState;
+        }
+    }
+
+    private IState currentState;
+    private IState previousState;
+    #endregion
+
+    #region Methods
+    public void ChangeState(IState newState)
+    {
+        if(currentState != null)
+        {
+            previousState = currentState;
+            currentState.Exit();
+        }
+        currentState = newState;
+        currentState.Enter();
+    }
+
+    public void ExecuteStateUpdate()
+    {
+        if(currentState != null)
+        {
+            currentState.Execute();
+        }
+    }
+    #endregion
+
+    public void ReturnToPreviousState()
+    {
+        if(currentState != null)
+        {
+            currentState.Exit();
+            currentState = previousState;
+            currentState.Enter();
+        }
+    }
+}
